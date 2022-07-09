2ce5b62214f313103f5af2f56ac38692ff6a6c5b
firker <56651759+firker@users.noreply.github.com>
2022-07-09 23:05:28 +0800

    Update diy1.sh

--- diy1.sh
+++ diy1.sh
@@ -14,7 +14,7 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
-# git clone https://github.com/fw876/helloworld.git package/ssr
+git clone https://github.com/fw876/helloworld.git package/ssr
 git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
