7824ea5a1aa0b590d3a8b8bab0fcc13d9c1de92b
firker <56651759+firker@users.noreply.github.com>
2024-09-02 21:31:50 +0800

    Update diy1.sh

--- diy1.sh
+++ diy1.sh
@@ -37,6 +37,7 @@ git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme
 git clone https://github.com/kiddin9/openwrt-bypass.git package/openwrt-bypass
 sed -i 's/luci-lib-ipkg/luci-base/g' package/openwrt-bypass/luci-app-bypass/Makefile
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06
